c4ece0b89245299728139c4f1c382a77bf271006
Giorgio <giorgioce@outlook.com>
2020-10-28 16:46:54 +0000

    Player 1 vs. Player 2

---
+++ spec/features/enter_names_spec.rb
@@ -0,0 +1,17 @@
+
+#
+# As two Players,
+# So we can play a personalised game of Battle,
+# We want to Start a fight by entering our names and seeing them
+
+
+feature 'Enter names' do
+  scenario 'submitting names' do
+    visit('/')
+    save_and_open
+    fill_in :player_1_name, with: 'player_1_name'
+    fill_in :player_2_name, with: 'player_2_name'
+    click_button 'Submit'
+    expect(page).to have_content "player_1_name vs. player_2_name"
+  end
+end
